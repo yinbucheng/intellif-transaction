4f27ae500a316dfb5629f792ce72f9d4cd2c7776
yinchong <yin.chong@intellif.com>
2018-10-11 14:05:04 +0800

    添加使用注解启动分布式事务

---
+++ src/main/java/cn/intellif/transaction/intelliftransaction/anotation/EnableIntellifTransaction.java
@@ -0,0 +1,20 @@
+package cn.intellif.transaction.intelliftransaction.anotation;
+
+import cn.intellif.transaction.intelliftransaction.aspect.DataSourceAspect;
+import cn.intellif.transaction.intelliftransaction.aspect.TxTransactionAspect;
+import cn.intellif.transaction.intelliftransaction.aware.ApplicationContextUtils;
+import cn.intellif.transaction.intelliftransaction.core.netty.NettyClient;
+import cn.intellif.transaction.intelliftransaction.interceptor.TransactionRequestInterceptor;
+import cn.intellif.transaction.intelliftransaction.listener.ServerListener;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import({DataSourceAspect.class, TxTransactionAspect.class, NettyClient.class, ServerListener.class, TransactionRequestInterceptor.class, ApplicationContextUtils.class})
+public @interface EnableIntellifTransaction {
+}
