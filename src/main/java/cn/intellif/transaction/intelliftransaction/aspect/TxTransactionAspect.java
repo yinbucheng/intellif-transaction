b8c33261bf7a8f137a85919c2b4bf5089c59b695
yinchong <yin.chong@intellif.com>
2018-10-11 15:39:41 +0800

    增加txtransaction的优先级让其优先于DataSource拦截器

--- src/main/java/cn/intellif/transaction/intelliftransaction/aspect/TxTransactionAspect.java
+++ src/main/java/cn/intellif/transaction/intelliftransaction/aspect/TxTransactionAspect.java
@@ -12,17 +12,27 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-public class TxTransactionAspect {
+public class TxTransactionAspect  implements Ordered {
 
     @Value("${intellif.transacton.timeout}")
     private Integer timeout;
 
     @Around("@annotation(cn.intellif.transaction.intelliftransaction.anotation.TxTransaction)")
-    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+    public Object aroundWithTx(ProceedingJoinPoint joinPoint) throws Throwable {
+        return runTxTransaction(joinPoint);
+    }
+
+    @Around("this(cn.intellif.transaction.intelliftransaction.anotation.TxTransaction) && execution( * *(..))")
+    public Object aroundWithTx2(ProceedingJoinPoint joinPoint) throws Throwable {
+        return runTxTransaction(joinPoint);
+    }
+
+    private Object runTxTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
         /**
          * 创建唯一标示
          */
@@ -47,25 +57,39 @@ public class TxTransactionAspect {
 
 
     @Around("@annotation(org.springframework.transaction.annotation.Transactional)")
-    public Object aroud(ProceedingJoinPoint joinPoint) throws Throwable {
-       Class clazz =  AopUtils.getTargetClass(joinPoint.getTarget());
-       if(clazz.getAnnotation(TxTransaction.class)!=null){
-           return joinPoint.proceed();
-       }
-      String token =   WebUtils.getRequest().getHeader(Constant.TRANSATION_TOKEN);
-       if(token==null){
-           return joinPoint.proceed();
-       }
-       TransactionConnUtils.initKey(token);
-       try{
-           //讲唯一表示告诉txManager
-           SocketManager.getInstance().sendMsg(ProtocolUtils.ping());
-           ConnectionTimeOutUtils.timeOut(TransactionConnUtils.getConnection(),timeout);
-           return joinPoint.proceed();
-       }catch (Exception e){
-           //发送异常信息告诉txManager
-           SocketManager.getInstance().sendMsg(ProtocolUtils.rollback());
-           throw new RuntimeException(e);
-       }
+    public Object aroundWithTr(ProceedingJoinPoint joinPoint) throws Throwable {
+        return runTransaction(joinPoint);
+    }
+
+    @Around("this(org.springframework.transaction.annotation.Transactional) && execution( * *(..))")
+    public Object aroundWithTR(ProceedingJoinPoint joinPoint) throws Throwable {
+        return runTransaction(joinPoint);
+    }
+
+    private Object runTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
+        Class clazz =  AopUtils.getTargetClass(joinPoint.getTarget());
+        if(clazz.getAnnotation(TxTransaction.class)!=null){
+            return joinPoint.proceed();
+        }
+        String token =   WebUtils.getRequest().getHeader(Constant.TRANSATION_TOKEN);
+        if(token==null){
+            return joinPoint.proceed();
+        }
+        TransactionConnUtils.initKey(token);
+        try{
+            //讲唯一表示告诉txManager
+            SocketManager.getInstance().sendMsg(ProtocolUtils.ping());
+            ConnectionTimeOutUtils.timeOut(TransactionConnUtils.getConnection(),timeout);
+            return joinPoint.proceed();
+        }catch (Exception e){
+            //发送异常信息告诉txManager
+            SocketManager.getInstance().sendMsg(ProtocolUtils.rollback());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public int getOrder() {
+        return HIGHEST_PRECEDENCE;
     }
 }
