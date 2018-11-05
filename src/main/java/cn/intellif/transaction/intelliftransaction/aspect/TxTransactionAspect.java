c6acdaa714ec9d511de63395a4de45ad16610126
yinchong <yin.chong@intellif.com>
2018-11-05 19:03:19 +0800

    修改其他地方调用出现HttpServletRequest中的空指针异常

--- src/main/java/cn/intellif/transaction/intelliftransaction/aspect/TxTransactionAspect.java
+++ src/main/java/cn/intellif/transaction/intelliftransaction/aspect/TxTransactionAspect.java
@@ -11,7 +11,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
